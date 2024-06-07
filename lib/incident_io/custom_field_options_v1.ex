7969ac7df0ac83658ec25ece5bcc0c5826cf2a36
Sasha Gerrand <sgerrand@users.noreply.github.com>
2024-06-07 12:29:58 +0100

    Add module and tests for the Create Custom Field Options V1 API

--- lib/incident_io/custom_field_options_v1.ex
+++ lib/incident_io/custom_field_options_v1.ex
@@ -4,6 +4,12 @@ defmodule IncidentIo.CustomFieldOptionsV1 do
   import IncidentIo
   alias IncidentIo.Client
 
+  @typep request_body :: %{
+           optional(:sort_key) => integer,
+           custom_field_id: binary,
+           value: binary
+         }
+
   @doc """
   Show custom field options for a custom field.
 
@@ -23,6 +29,33 @@ defmodule IncidentIo.CustomFieldOptionsV1 do
         custom_field_id: custom_field_id,
         page_size: page_size
       }
+    )
+  end
+
+  @doc """
+  Create a custom field option.
+
+  Custom field option body example:
+  ```elixir
+  %{
+    custom_field_id: "01FCNDV6P870EA6S7TK1DSYDG0",
+    sort_key: 10,
+    value: "Product"
+  }
+  ```
+
+  ## Example
+
+      IncidentIo.CustomFieldOptionsV1.create(client, body)
+
+  More information at: https://api-docs.incident.io/tag/Custom-Field-Options-V1#operation/Custom%20Field%20Options%20V1_Create
+  """
+  @spec create(Client.t(), request_body()) :: IncidentIo.response()
+  def create(client \\ %Client{}, body) do
+    post(
+      "v2/custom_field_options",
+      client,
+      body
     )
   end
 end
--- test/custom_field_options_v1_test.exs
+++ test/custom_field_options_v1_test.exs
@@ -30,4 +30,32 @@ defmodule IncidentIo.CustomFieldOptionsV1Test do
       end
     end
   end
+
+  describe "create/2" do
+    @body %{
+      custom_field_id: "01FCNDV6P870EA6S7TK1DSYDG0",
+      sort_key: 10,
+      value: "Product"
+    }
+
+    test "returns expected HTTP status code" do
+      use_cassette "custom_field_options_v1#create_entry" do
+        assert {201, _, _} = create(@client, @body)
+      end
+    end
+
+    test "returns expected response" do
+      use_cassette "custom_field_options_v1#create_entry" do
+        {201, %{custom_field_option: custom_field_option}, _} =
+          create(@client, @body)
+
+        assert %{
+                 custom_field_id: "01FCNDV6P870EA6S7TK1DSYDG0",
+                 id: "01FCNDV6P870EA6S7TK1DSYDG0",
+                 sort_key: 10,
+                 value: "Product"
+               } == custom_field_option
+      end
+    end
+  end
 end
