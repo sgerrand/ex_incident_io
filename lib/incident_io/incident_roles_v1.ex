855bec60ed5cc8b68705d2dbb886c3c488b81b9a
Sasha Gerrand <sgerrand@users.noreply.github.com>
2024-06-09 17:29:10 +0100

    Fix param in example

--- lib/incident_io/incident_roles_v1.ex
+++ lib/incident_io/incident_roles_v1.ex
@@ -45,7 +45,7 @@ defmodule IncidentIo.IncidentRolesV1 do
 
   ## Example
 
-      IncidentIo.IncidentRolesV1.create(client, "some-incident-id")
+      IncidentIo.IncidentRolesV1.create(client, body)
 
   More information at: https://api-docs.incident.io/tag/Incident-Roles-V1#operation/Incident-Roles%20V1_Create
   """
