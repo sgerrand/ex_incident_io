c50c28cdfa1362463fdbecaee9ac320c234ffd19
Sasha Gerrand <sgerrand@users.noreply.github.com>
2024-06-09 17:06:56 +0100

    Add module and tests for the Create Incident Roles V1 API

--- lib/incident_io/incident_roles_v1.ex
+++ lib/incident_io/incident_roles_v1.ex
@@ -4,6 +4,14 @@ defmodule IncidentIo.IncidentRolesV1 do
   import IncidentIo
   alias IncidentIo.Client
 
+  @typep request_body :: %{
+           description: nonempty_binary,
+           instructions: binary,
+           name: nonempty_binary,
+           required: bool,
+           shortform: nonempty_binary
+         }
+
   @doc """
   List all incident roles for an organisation.
 
@@ -18,6 +26,35 @@ defmodule IncidentIo.IncidentRolesV1 do
     get(
       "v1/incident_roles",
       client
+    )
+  end
+
+  @doc """
+  Create a new incident role.
+
+  Incident role body example:
+  ```elixir
+  %{
+    description: "The person currently coordinating the incident",
+    instructions: "Take point on the incident; Make sure people are clear on responsibilities",
+    name: "Incident Lead",
+    required: false,
+    shortform: "lead"
+  }
+  ```
+
+  ## Example
+
+      IncidentIo.IncidentRolesV1.create(client, "some-incident-id")
+
+  More information at: https://api-docs.incident.io/tag/Incident-Roles-V1#operation/Incident-Roles%20V1_Create
+  """
+  @spec create(Client.t(), request_body()) :: IncidentIo.response()
+  def create(client \\ %Client{}, body) do
+    post(
+      "v1/incident_roles",
+      client,
+      body
     )
   end
 end
--- test/incident_roles_v1_test.exs
+++ test/incident_roles_v1_test.exs
@@ -36,4 +36,41 @@ defmodule IncidentIo.IncidentRolesV1Test do
       end
     end
   end
+
+  describe "create/3" do
+    @body %{
+      description: "The person currently coordinating the incident",
+      instructions: "Take point on the incident; Make sure people are clear on responsibilities",
+      name: "Incident Lead",
+      required: false,
+      shortform: "lead"
+    }
+
+    test "returns expected HTTP status code" do
+      use_cassette "incident_roles_v1#create" do
+        assert {201, _, _} = create(@client, @body)
+      end
+    end
+
+    test "returns expected response" do
+      use_cassette "incident_roles_v1#create" do
+        {201, response, _} = create(@client, @body)
+
+        assert %{
+                 incident_role: %{
+                   created_at: "2021-08-17T13:28:57.801578Z",
+                   description: "The person currently coordinating the incident",
+                   id: "01FCNDV6P870EA6S7TK1DSYDG0",
+                   instructions:
+                     "Take point on the incident; Make sure people are clear on responsibilities",
+                   name: "Incident Lead",
+                   required: false,
+                   role_type: "lead",
+                   shortform: "lead",
+                   updated_at: "2021-08-17T13:28:57.801578Z"
+                 }
+               } == response
+      end
+    end
+  end
 end
