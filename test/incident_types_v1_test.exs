3e302a4e19686fe7a95011c72c22bf76a6369c7b
Sasha Gerrand <sgerrand@users.noreply.github.com>
2024-06-10 11:11:33 +0100

    Add module and tests for the Incident Types V1 API

---
+++ test/incident_types_v1_test.exs
@@ -0,0 +1,64 @@
+defmodule IncidentIo.IncidentTypesV1Test do
+  use IncidentIo.TestCase, async: true
+  import IncidentIo.IncidentTypesV1
+
+  doctest IncidentIo.IncidentTypesV1
+
+  @client IncidentIo.Client.new(%{api_key: "yourtokengoeshere"})
+
+  describe "list/1" do
+    test "returns expected HTTP status code" do
+      use_cassette "incident_types_v1#list" do
+        assert {200, _, _} = list(@client)
+      end
+    end
+
+    test "returns expected response" do
+      use_cassette "incident_types_v1#list" do
+        {200, response, _} = list(@client)
+
+        assert %{
+                 incident_types: [
+                   %{
+                     create_in_triage: "always",
+                     created_at: "2021-08-17T13:28:57.801578Z",
+                     description: "Customer facing production outages",
+                     id: "01FCNDV6P870EA6S7TK1DSYDG0",
+                     is_default: false,
+                     name: "Production Outage",
+                     private_incidents_only: false,
+                     updated_at: "2021-08-17T13:28:57.801578Z"
+                   }
+                 ]
+               } == response
+      end
+    end
+  end
+
+  describe "show/2" do
+    test "returns expected HTTP status code" do
+      use_cassette "incident_types_v1#show" do
+        assert {200, _, _} = show(@client, "01FCNDV6P870EA6S7TK1DSYDG0")
+      end
+    end
+
+    test "returns expected response" do
+      use_cassette "incident_types_v1#show" do
+        {200, response, _} = show(@client, "01FCNDV6P870EA6S7TK1DSYDG0")
+
+        assert %{
+                 incident_type: %{
+                   create_in_triage: "always",
+                   created_at: "2021-08-17T13:28:57.801578Z",
+                   description: "Customer facing production outages",
+                   id: "01FCNDV6P870EA6S7TK1DSYDG0",
+                   is_default: false,
+                   name: "Production Outage",
+                   private_incidents_only: false,
+                   updated_at: "2021-08-17T13:28:57.801578Z"
+                 }
+               } == response
+      end
+    end
+  end
+end
