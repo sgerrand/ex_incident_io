df38063511f5a8fe9a16fe12abde96a8cbab1039
Sasha Gerrand <sgerrand@users.noreply.github.com>
2024-07-03 13:40:33 +0100

    Use dev postfix for versions on main branch
    On reflection I think that it's better to tag package releases from
    long lived branches instead of using the trunk branch for everything.
    This approach seems saner for developing and maintaining libraries or
    packages.

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule IncidentIo.Mixfile do
   use Mix.Project
 
   @source_url "https://github.com/sgerrand/ex_incident_io"
-  @version "0.1.0"
+  @version "0.1.0-dev"
 
   def project do
     [
